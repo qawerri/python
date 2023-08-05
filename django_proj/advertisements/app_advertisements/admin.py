d22515997405f1850e51482e77da68c34a581ac6
qawerri <qawerri@yandex.ru>
2023-08-05 16:06:10 +0300

    initial

--- django_proj/advertisements/app_advertisements/admin.py
+++ django_proj/advertisements/app_advertisements/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_at', 'created_date']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_true', 'make_auction_false']
+
+    @admin.action(description='Перевести auction в True')
+    def make_auction_true(self, request, query):
+        query.update(auction=True)
+
+    @admin.action(description='Перевести auction в False')
+    def make_auction_false(self, request, query):
+        query.update(auction=False)
+
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description'),
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- django_proj/advertisements/app_advertisements/models.py
+++ django_proj/advertisements/app_advertisements/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128, )
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, уместен ли торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
+
+
+
+
+# https://docs.djangoproject.com/en/4.2/ref/models/fields/
